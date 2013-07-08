e02c7c2b2564a05f25cb37736acbbb814b3fb978
flycoo <460506001@qq.com>
2013-07-08 16:43:58 +0800

    for test
    for test

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    char *h = "s你好";
+    cout<<strlen(h);
+
+     return 0;
+}
